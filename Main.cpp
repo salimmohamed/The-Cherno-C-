ec2d2916d6bbf2da97b35033b09a8c89789b9961
salimmohamed <mohamsal@oregonstate.edu>
2024-11-20 19:24:09 -0800

    Finished video #10: C++ Header Files

--- Main.cpp
+++ Main.cpp
@@ -1,21 +1,9 @@
 #include <iostream>
-
-int Multiply(int a, int b)
-{
-	return a * b;
-}
-
-void MultiplyAndLog(int a, int b)
-{
-	int result = Multiply(a, b);
-	std::cout << result << std::endl;
-}
+#include "Log.h"
 
 int main()
 {
-	MultiplyAndLog(3, 2);
-	MultiplyAndLog(8, 5);
-	MultiplyAndLog(90, 45);
-
+	InitLog();
+	Log("Hello World!");
 	std::cin.get();
 }
