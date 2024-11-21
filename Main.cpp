126db3fbb5a39d9a694ef5ea22458f4472b4d36d
salimmohamed <mohamsal@oregonstate.edu>
2024-11-20 19:02:54 -0800

    Finished video #9: Functions in C++

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 
-void Log(const char* message);
+int Multiply(int a, int b)
+{
+	return a * b;
+}
+
+void MultiplyAndLog(int a, int b)
+{
+	int result = Multiply(a, b);
+	std::cout << result << std::endl;
+}
 
 int main()
 {
-	bool variable = false;
+	MultiplyAndLog(3, 2);
+	MultiplyAndLog(8, 5);
+	MultiplyAndLog(90, 45);
 
-	std::cout << sizeof(bool) << std::endl;
 	std::cin.get();
 }
